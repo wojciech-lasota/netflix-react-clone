5f26734ae5c2bcdcd3ae646af37ef629443cf087
bax <dev.lasota@gmail.com>
2022-11-01 19:52:58 +0100

    authentication flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
+import { login, logout, selectUser } from "./features/userSlice";
 import { auth } from "./firebase";
 import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-  const users = null;
+  const users = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
         return;
       }
       // logout
+      dispatch(logout());
     });
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
   return (
     <div className="app">
       <Router>
@@ -27,6 +37,7 @@ function App() {
         ) : (
           <Routes>
             <Route exact path="/" element={<HomeScreen />}></Route>
+            <Route exact path="/profile" element={<ProfileScreen />}></Route>
             <Route exact path="/login" element={<Login />}></Route>
           </Routes>
         )}
